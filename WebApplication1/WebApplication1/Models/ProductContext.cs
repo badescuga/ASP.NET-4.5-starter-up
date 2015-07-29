49b87deaa3b1b84cab683d7c86da98e6a1318ea2
Gabriel-Alexandru Badescu <a-gabade@microsoft.com>
2015-07-29 16:15:38 +0300

    first tutorial step: created the EF context and setting up db stuff

---
+++ WebApplication1/WebApplication1/Models/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToys")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
